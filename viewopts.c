1926ae8171a97515943e8bcc79dd37fb862a52a6
Jeff Minard <jeff@creditkarma.com>
2013-01-31 09:25:00 -0800

    Allow the extension to recognize and pass the "include_docs" parametere for views

--- viewopts.c
+++ viewopts.c
@@ -54,6 +54,7 @@ static view_param Recognized_View_Params[] = {
 	{ "startkey", jval_param_handler },
 	{ "startkey_docid", string_param_handler },
 	{ "debug", bool_param_handler },
+	{ "include_docs", bool_param_handler },
 	{ NULL, NULL }
 };
 
